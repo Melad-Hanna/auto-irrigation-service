ac4c45a940364bb3eddfd914efbf820bb3f9b255
meladhanna <eng.meladhanna10@yahoo.com>
2023-05-12 23:05:32 +0300

    Initial commit, add maven dependencies

---
+++ src/main/java/com/banquemisr/autoirrigationservice/AutoIrrigationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banquemisr.autoirrigationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoIrrigationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoIrrigationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banquemisr/autoirrigationservice/AutoIrrigationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquemisr.autoirrigationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoIrrigationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
